67c4e74914d361fbccd1fa88c8d14862abb17c1a
Alba Sornosa <alba.sornosa@devterm.de>
2024-02-12 15:44:10 +0100

    feat: create note stack component

---
+++ src/app/components/note-stack/note-stack.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NoteComponent } from '../note/note.component';
+import { NoteStackComponent } from './note-stack.component';
+
+describe('NoteStackComponent', () => {
+  let component: NoteStackComponent;
+  let fixture: ComponentFixture<NoteStackComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NoteStackComponent, NoteComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(NoteStackComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/note-stack/note-stack.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Note } from '../../types/note';
+import { DateUtil } from '../../utils/date.util';
+import { NoteComponent } from '../note/note.component';
+
+@Component({
+  selector: 'app-note-stack',
+  standalone: true,
+  imports: [NoteComponent],
+  templateUrl: './note-stack.component.html',
+  styleUrl: './note-stack.component.scss',
+})
+export class NoteStackComponent {
+  @Input() notes: Note[] = [];
+  @Output() noteSelected = new EventEmitter<Note>();
+
+  handleSelection(note: Note): void {
+    this.noteSelected.emit(note);
+  }
+
+  getNoteWidth(note: Note): number {
+    const duration = DateUtil.daysInRange(note.startDate, note.endDate);
+    return duration <= 1 ? 85 : 100 * (duration - 1) + 85;
+  }
+}
